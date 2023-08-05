4ce8fd6a635df2f60dca326312fe953b046f8f1f
Bazsalanszky <balazs@toldi.eu>
2023-08-05 19:53:49 +0200

    Fix view all comments button

--- app/src/main/java/eu/toldi/infinityforlemmy/fragments/ViewPostDetailFragment.java
+++ app/src/main/java/eu/toldi/infinityforlemmy/fragments/ViewPostDetailFragment.java
@@ -1231,7 +1231,7 @@ public class ViewPostDetailFragment extends Fragment implements FragmentCommunic
                                 mPostDetailsSharedPreferences, mExoCreator,
                                 post1 -> EventBus.getDefault().post(new PostUpdateEventToPostList(mPost, postListPosition)));
                         mSwipeRefreshLayout.setRefreshing(false);
-                        FetchComment.fetchComments(mExecutor, new Handler(), mRetrofit.getRetrofit(), mAccessToken, post.getId(), mSingleCommentId == 0 ? null : mSingleCommentParentId == 0 ? mSingleCommentId : mSingleCommentParentId, sortType, mExpandChildren, 1, new FetchComment.FetchCommentListener() {
+                        FetchComment.fetchComments(mExecutor, new Handler(), mRetrofit.getRetrofit(), mAccessToken, post.getId(), mSingleCommentId == null || mSingleCommentId == 0 ? null : mSingleCommentParentId == 0 ? mSingleCommentId : mSingleCommentParentId, sortType, mExpandChildren, 1, new FetchComment.FetchCommentListener() {
                             @Override
                             public void onFetchCommentSuccess(ArrayList<Comment> expandedComments, Integer parentId, ArrayList<Integer> children) {
                                 pages_loaded++;
@@ -1350,7 +1350,7 @@ public class ViewPostDetailFragment extends Fragment implements FragmentCommunic
         }
 
 
-        FetchComment.fetchComments(mExecutor, new Handler(), mRetrofit.getRetrofit(), mAccessToken, mPost.getId(), mSingleCommentId == 0 ? null : mSingleCommentParentId == 0 ? mSingleCommentId : mSingleCommentParentId, sortType, mExpandChildren, pages_loaded + 1,
+        FetchComment.fetchComments(mExecutor, new Handler(), mRetrofit.getRetrofit(), mAccessToken, mPost.getId(), mSingleCommentId == null || mSingleCommentId == 0 ? null : mSingleCommentParentId == 0 ? mSingleCommentId : mSingleCommentParentId, sortType, mExpandChildren, pages_loaded + 1,
                 new FetchComment.FetchCommentListener() {
                     @Override
                     public void onFetchCommentSuccess(ArrayList<Comment> expandedComments,
